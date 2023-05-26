f766060bee480c06058c7ffa1e7d7f68951dc796
Lynn Wolters <lynn.wolters27@gmail.com>
2023-05-26 18:09:22 +0200

    afronden

--- src/main.js
+++ src/main.js
@@ -5,8 +5,22 @@ import './styles/marquee.scss'
 import './styles/mobile-menu.scss'
 import './styles/underline-hover.scss'
 import './styles/submenu.scss'
+import './styles/swup.scss'
 
 // SCRIPTS
 import './js/cursor.js'
+
+// SWUP 
+import Swup from 'swup'
+
+const swup = new Swup({
+})
+
+document.addEventListener('animationOutDone', function() {
+    
+})
+
+
+
 
 
